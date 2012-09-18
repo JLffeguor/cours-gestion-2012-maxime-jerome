718d21e9361625dbec37287217f0cacb473df6d7
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-09-18 17:50:56 +0000

    useer class

---
+++ 18-09-2012/src/User.java
@@ -0,0 +1,37 @@
+
+public class User {
+	
+	private String matricule;
+	private String adress;
+	private int childrenNumber;
+	private int phone;
+	
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getChildrenNumber() {
+		return childrenNumber;
+	}
+	public void setChildrenNumber(int childrenNumber) {
+		this.childrenNumber = childrenNumber;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
