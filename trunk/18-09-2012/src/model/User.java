c0c897f8792e855b5c8ef0d5e7ce0fe6bc098815
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-09-19 08:08:10 +0000

    create package model and repository

---
+++ trunk/18-09-2012/src/model/User.java
@@ -0,0 +1,42 @@
+package model;
+
+public class User extends BaseEntity{
+	
+	private String matricule;
+	private String adress;
+	private int childrenNumber;
+	private int phone;
+	
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getChildrenNumber() {
+		return childrenNumber;
+	}
+	public void setChildrenNumber(int childrenNumber) {
+		this.childrenNumber = childrenNumber;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [matricule=" + matricule + ", adress=" + adress
+				+ ", childrenNumber=" + childrenNumber + ", phone=" + phone
+				+ "]";
+	}	
+}
