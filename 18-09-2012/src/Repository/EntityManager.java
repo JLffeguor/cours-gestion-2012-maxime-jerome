b6b66135eec982f068c08471e8ca60433dcfd82d
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-09-19 08:09:23 +0000

    create userrepository and entitymanager

---
+++ 18-09-2012/src/Repository/EntityManager.java
@@ -0,0 +1,39 @@
+package Repository;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import model.BaseEntity;
+
+
+public class EntityManager<E extends BaseEntity> {
+	
+	private String ENCODING="UTF-8";
+	private Path path = null;
+
+	public EntityManager(String filePath) {
+		path = FileSystems.getDefault().getPath(filePath);
+		if(Files.notExists(path)){
+			try {
+				Files.createFile(path);
+			} catch (IOException ioe) {
+				throw new RuntimeException(this.getClass().getName()+" can't create the persistence file ",ioe);
+			}
+		}
+	}
+	
+	public E persist(E entity)
+	{
+		try (BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName(ENCODING),StandardOpenOption.WRITE,StandardOpenOption.APPEND)) {
+		    writer.write(entity.toString());
+		} catch (IOException ioe) {
+		    throw new RuntimeException(this.getClass().getName()+" can't persist ",ioe);
+		}
+		return entity;
+	}
+
+}
