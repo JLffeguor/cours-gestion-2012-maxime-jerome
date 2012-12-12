2a3f0e7af100815473e34a14a3da144999100dd3
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-12 01:28:02 +0000

    launcheck method winecave.java

--- trunk/WineCaveSwing/src/be/winecave/main/WineCave.java
+++ trunk/WineCaveSwing/src/be/winecave/main/WineCave.java
@@ -1,11 +1,20 @@
 package be.winecave.main;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import be.winecave.util.AppConfig;
 
 public class WineCave {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		launchCheck();
+	}
+	
+	////code pour la première éxécution du porgramme , doit être porté dans une classe spécifique? --maxime 12/12/12
+	public static void launchCheck(){
+		
+		
+		if( AppConfig.isFirstExecution() ) {
+			//TODO remplir la db ici
+		}
 	}
 }
