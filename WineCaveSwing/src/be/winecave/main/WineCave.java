74e64dbb1a586e9b010c46b20c7b871e7de34f2e
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-23 16:48:20 +0000

    can save payx.xml into db

--- WineCaveSwing/src/be/winecave/main/WineCave.java
+++ WineCaveSwing/src/be/winecave/main/WineCave.java
@@ -9,19 +9,21 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import be.winecave.Repository.TestService;
+import be.winecave.installation.PostInstallationPays;
 import be.winecave.util.AppConfig;
 import be.winecave.util.FileUtil;
 
 public class WineCave {
 	private static Log log = LogFactory.getLog(FileUtil.class);
+	private static ApplicationContext context;
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
 		if(log.isDebugEnabled()) {
 			debuggingPurpose();
 		}
 		
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		launchCheck();
 	}
 	
@@ -31,6 +33,10 @@ public class WineCave {
 	}
 	
 	public static void launchCheck(){
+		////TODO remove --maxime 23/12/12
+		PostInstallationPays postInstallationPays = (PostInstallationPays) context.getBean("postInstallationPays");
+		postInstallationPays.loadDataFromXml();
+		postInstallationPays.saveDataToDb();
 		
 		if( AppConfig.isFirstExecution() ) {
 			//TODO remplir la db ici avec les données post-install
