6a48870e4c63e7619ccddc65193925b4974e07dd
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-23 15:19:50 +0000

    can load xml pays

---
+++ trunk/WineCaveSwing/test/XmlParsing.java
@@ -0,0 +1,10 @@
+import be.winecave.installation.PostInstallationPays;
+
+
+public class XmlParsing {
+	
+	public static void main(String[]args) {
+		PostInstallationPays.getInstance().loadDataFromXml();
+	}
+
+}
