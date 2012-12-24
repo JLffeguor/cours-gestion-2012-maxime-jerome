885e9bcb4860a91fbb2e338dbaf218c971e43d7c
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-24 21:52:14 +0000

    move app.properties => Config/app.properties. update FileUtils.java and appConfig.java
    lazy init of postinstallationpays.java
    
    sompe refactoring in FileUtil.java and XmlUtil.java

--- WineCaveSwing/src/be/winecave/installation/PostInstallationPays.java
+++ WineCaveSwing/src/be/winecave/installation/PostInstallationPays.java
@@ -11,23 +11,27 @@ import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import be.winecave.Repository.PaysViticoleRepository;
 import be.winecave.model.PaysViticole;
 import be.winecave.util.FileUtil;
 import be.winecave.util.XmlUtil;
 
-@Service
+@Component
+@Lazy
 @Scope("singleton")
 public class PostInstallationPays implements PostInstallData<List<PaysViticole>> {
 	
-	@Autowired 
+	@Autowired
 	PaysViticoleRepository paysViticoleRepository;
 	
-	private static final String XmlFileName =  ("pays.xml");
-	private static Document xmlDocument =  XmlUtil.getXmlDocumentIntoInstallationDirectory(XmlFileName);
+	private static final String XML_FILE_NAME =  ("pays.xml");
+	private static final String ELEMENTS_NAME = "pays";
+	private static final String ROOT_ELEMENTS_NAME = "liste_pays";
+	private static Document xmlDocument =  XmlUtil.getPostInstallXmlDocument(XML_FILE_NAME);
 	
 	private List<PaysViticole> listePays = new ArrayList<>();
 
@@ -55,7 +59,7 @@ public class PostInstallationPays implements PostInstallData<List<PaysViticole>>
 		
 		XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
 		try {
-			xmlOutput.output(doc, Files.newOutputStream(FileUtil.getFilePathIntoInstallationFolder("PostInstallationData" + FileUtil.getFileSystemSeparator() + XmlFileName), StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING));
+			xmlOutput.output(doc, Files.newOutputStream(FileUtil.getPathIntoInstallationFolder("PostInstallationData" + FileUtil.getFileSystemSeparator() + XML_FILE_NAME), StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING));
 		} catch (IOException e) {
 			throw new RuntimeException("can't save pays.xml",e);
 		}
--- WineCaveSwing/src/be/winecave/util/XmlUtil.java
+++ WineCaveSwing/src/be/winecave/util/XmlUtil.java
@@ -9,18 +9,18 @@ import org.jdom2.input.SAXBuilder;
 
 public class XmlUtil {
 	
-	public static Document getXmlDocumentIntoInstallationDirectory(String fileName) {
-		return getXmlDocument(FileUtil.getFilePathIntoInstallationFolder("PostInstallationData" + FileUtil.getFileSystemSeparator() + fileName));
+	public static Document getPostInstallXmlDocument(String fileName) {
+		return getXmlDocument(FileUtil.getPathIntoInstallationFolder("PostInstallationData" + FileUtil.getFileSystemSeparator() + fileName));
 	}
 	
 	public static Document getXmlDocument(Path filePath) {
 		SAXBuilder builder = new SAXBuilder();
 		
 		try {
-			return builder.build(filePath.toFile());
+			return builder.build(filePath.toUri().toURL());
 		} catch (JDOMException | IOException e) {
 			throw new RuntimeException("can't get xmlDocument",e);
 		}
 	}
-
+	
 }
