dcd311a23d98db292bf30fa8cb311b34c30edd6e
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-24 21:54:50 +0000

    manual export of db schema for first execution .
    first execution db insert executes in a separate spring context (and destroyed after)
    
    add debug flag

--- WineCaveSwing/src/be/winecave/main/WineCave.java
+++ WineCaveSwing/src/be/winecave/main/WineCave.java
@@ -2,43 +2,100 @@ package be.winecave.main;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.jar.JarFile;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import be.winecave.installation.PostInstallationPays;
+import be.winecave.model.PaysViticole;
 import be.winecave.util.AppConfig;
 import be.winecave.util.FileUtil;
+import be.winecave.util.JarUtils;
 
 public class WineCave {
 	private static Log log = LogFactory.getLog(FileUtil.class);
 	private static ApplicationContext context;
+	
+	private static boolean debugMode;
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
+		launchCheck();
+		
 		if(log.isDebugEnabled()) {
 			debuggingPurpose();
 		}
 		
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		launchCheck();
+		
 	}
 	
 	public static void debuggingPurpose(){
 		Properties props = System.getProperties();
 		props.list(System.out);
+		
+		debugMode = true;
 	}
 	
-	public static void launchCheck(){
-		////TODO remove --maxime 23/12/12
-		PostInstallationPays postInstallationPays = (PostInstallationPays) context.getBean("postInstallationPays");
-		postInstallationPays.loadDataFromXml();
-		postInstallationPays.saveDataToDb();
+	@SuppressWarnings("deprecation")
+	public static void ExportDbSchema(boolean dropBefore) {
+		// Empty map. We add no additional property, everything is already in the persistence.xml
+		Map<String,Object> map=new HashMap<String,Object>(); 
+		// Get the config from the persistence.xml file, with the unit name as parameter.
+		Ejb3Configuration conf =  new Ejb3Configuration().configure("ConnectionMySql",map);
+		SchemaExport schemaExport = new SchemaExport(conf.getHibernateConfiguration());
+		
+		if(dropBefore) {
+			schemaExport.drop(debugMode, true);
+		}
+		schemaExport.create(debugMode, true);
+	}
+	
+	public static void launchCheck() throws URISyntaxException, IOException{
+		JarFile jar = null;
+		if ( (jar = JarUtils.jarForClass(WineCave.class, null)) != null ) {
+			System.out.println("iam in jar");
+			System.out.println(jar.getName());
+			//we must copy ressource out now
+			//nooooooooooooooooooooooooooooowwwwwwwwwwwwwwwwww
+			//...
+			//okay
+			JarUtils.addRessourcesNextJar(jar, "PostInstallationData", false);
+			JarUtils.addRessourcesNextJar(jar, "Config", false);
+			
+		} else  {
+			System.out.println("i am not in a jar");
+		}
+		
 		
 		if( AppConfig.isFirstExecution() ) {
+			System.out.println("this is first execution");
+			
+			ExportDbSchema(false);
+			
+			context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			
 			//TODO remplir la db ici avec les données post-install
+			
+			PostInstallationPays postInstallationPays = (PostInstallationPays) context.getBean("postInstallationPays");
+			postInstallationPays.loadDataFromXml();
+			postInstallationPays.saveDataToDb();
+			postInstallationPays.getData().add(new PaysViticole("test"));
+			
+			postInstallationPays.saveDataToXml();
+
+			////destruction forcé du spring context pour déchargé les classe postinstallation de la mémoire (nous n'en avons plus besoin)
+			((AbstractApplicationContext) context).registerShutdownHook();
+			context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		}
 	}
 }
