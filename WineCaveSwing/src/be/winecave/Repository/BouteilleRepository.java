f139cce12d68c73ac52ecf9455bc069a4d7207ce
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-27 02:26:40 +0000

    new repositories for bouteille,categorie,classement,couleur

---
+++ WineCaveSwing/src/be/winecave/Repository/BouteilleRepository.java
@@ -0,0 +1,9 @@
+package be.winecave.Repository;
+
+import org.springframework.stereotype.Repository;
+
+import be.winecave.model.Bouteille;
+@Repository
+public class BouteilleRepository extends BaseRepository<Bouteille> {
+
+}
