c520371690c15272e3c497380feb1296e92eb3d4
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-27 02:29:59 +0000

    some refator and postinstallation savetodb call added

--- WineCaveSwing/src/be/winecave/main/WineCave.java
+++ WineCaveSwing/src/be/winecave/main/WineCave.java
@@ -1,6 +1,7 @@
 package be.winecave.main;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,17 +10,19 @@ import java.util.jar.JarFile;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.ejb.Ejb3Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import be.winecave.installation.PostInstallationBouteille;
+import be.winecave.installation.PostInstallationCategorie;
+import be.winecave.installation.PostInstallationClassement;
+import be.winecave.installation.PostInstallationCouleur;
 import be.winecave.installation.PostInstallationPays;
 import be.winecave.installation.PostInstallationRegionViticole;
-import be.winecave.model.PaysViticole;
-import be.winecave.model.RegionViticole;
+import be.winecave.installation.PostInstallationSousRegionViticole;
 import be.winecave.util.AppConfig;
 import be.winecave.util.FileUtil;
 import be.winecave.util.JarUtils;
@@ -31,25 +34,76 @@ public class WineCave {
 	private static boolean debugMode;
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		launchCheck();
 		
-		if(log.isDebugEnabled()) {
-			debuggingPurpose();
+		if(log.isDebugEnabled()) { //TODO check console arg
+			activeDebugMode();
 		}
+		activeDebugMode();//TODO remove it
+		
+		launchCheck();
 		
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 	}
 	
-	public static void debuggingPurpose(){
+	private static void activeDebugMode(){
+		System.out.println("java system properties");
 		Properties props = System.getProperties();
 		props.list(System.out);
 		
 		debugMode = true;
 	}
 	
+	private static void launchCheck() {
+		
+		ensureRessourcesInstalled();
+		
+		if( AppConfig.isFirstExecution() ) {
+			log.debug("this is first execution");
+			
+			ExportDbSchema(true);//TODO set to false in prod --maxime 24/12/12
+			
+			context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			
+			saveToDbPostIstallationData();
+			
+			////destruction forcé du spring context pour déchargé les classe postinstallation de la mémoire (nous n'en avons plus besoin)
+			((AbstractApplicationContext) context).registerShutdownHook();
+			context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		}
+	}
+	
+	private static void ensureRessourcesInstalled() {
+		JarFile jar = null;
+		
+		try {
+			jar = JarUtils.jarForClass(WineCave.class, null);
+		} catch (MalformedURLException | URISyntaxException e) {
+			throw new RuntimeException("can't detect if the app is in a jar or not ", e);
+		}
+		
+		
+		if ( jar != null ) {
+			log.debug("iam in jar");
+			//we must copy ressource out now
+			//nooooooooooooooooooooooooooooowwwwwwwwwwwwwwwwww
+			//...
+			//okay
+			
+			try {
+				JarUtils.addRessourcesNextJar(jar, "PostInstallationData", false);
+				JarUtils.addRessourcesNextJar(jar, "Config", false);
+			} catch (IOException e) {
+				throw new RuntimeException("can't install Ressource next jar ", e);
+			}
+			
+		} else  {
+			log.debug("i am not in a jar");
+		}
+	}
+	
 	@SuppressWarnings("deprecation")
-	public static void ExportDbSchema(boolean dropBefore) {
+	private static void ExportDbSchema(boolean dropBefore) {
 		// Empty map. We add no additional property, everything is already in the persistence.xml
 		Map<String,Object> map=new HashMap<String,Object>(); 
 		// Get the config from the persistence.xml file, with the unit name as parameter.
@@ -62,37 +116,16 @@ public class WineCave {
 		schemaExport.create(debugMode, true);
 	}
 	
-	public static void launchCheck() throws URISyntaxException, IOException{
-		JarFile jar = null;
-		if ( (jar = JarUtils.jarForClass(WineCave.class, null)) != null ) {
-			System.out.println("iam in jar");
-			System.out.println(jar.getName());
-			//we must copy ressource out now
-			//nooooooooooooooooooooooooooooowwwwwwwwwwwwwwwwww
-			//...
-			//okay
-			JarUtils.addRessourcesNextJar(jar, "PostInstallationData", false);
-			JarUtils.addRessourcesNextJar(jar, "Config", false);
-			
-		} else  {
-			System.out.println("i am not in a jar");
-		}
-		
-		
-		if( AppConfig.isFirstExecution() ) {
-			System.out.println("this is first execution");
-			
-			ExportDbSchema(true);//TODO set to false in prod --maxime 24/12/12
-			
-			context = new ClassPathXmlApplicationContext("applicationContext.xml");
-			
-			//TODO remplir la db ici avec les données post-install
-			
-			((PostInstallationPays) context.getBean("postInstallationPays")).saveDataToDb();
-			((PostInstallationRegionViticole) context.getBean("postInstallationRegionViticole")).saveDataToDb();
-			////destruction forcé du spring context pour déchargé les classe postinstallation de la mémoire (nous n'en avons plus besoin)
-			((AbstractApplicationContext) context).registerShutdownHook();
-			context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		}
+	private static void saveToDbPostIstallationData() {
+		//assuming spring context already created
+		((PostInstallationPays) context.getBean("postInstallationPays")).saveDataToDb();
+		((PostInstallationRegionViticole) context.getBean("postInstallationRegionViticole")).saveDataToDb();
+		((PostInstallationSousRegionViticole) context.getBean("postInstallationSousRegionViticole")).saveDataToDb();
+		((PostInstallationBouteille) context.getBean("postInstallationBouteille")).saveDataToDb();
+		((PostInstallationCategorie) context.getBean("postInstallationCategorie")).saveDataToDb();
+		((PostInstallationClassement) context.getBean("postInstallationClassement")).saveDataToDb();
+		((PostInstallationCouleur) context.getBean("postInstallationCouleur")).saveDataToDb();
 	}
+	
+
 }
