e0152fd10baa936bca3c84a67e3dc4b4c8e47323
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-27 11:23:36 +0000

    package Repository => repository

--- trunk/WineCaveSwing/src/be/winecave/Repository/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package be.winecave.Repository;
-
-import org.springframework.stereotype.Repository;
-
-import be.winecave.model.Categorie;
-@Repository
-public class CategorieRepository extends BaseRepository<Categorie> {
-
-}
