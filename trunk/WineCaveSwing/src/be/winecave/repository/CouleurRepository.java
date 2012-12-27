73de723b9633e3e731967b1ba2dd28d1739c8733
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-27 12:10:20 +0000

    add findbyname method to repo bouteille,cat.,classement and couleur

--- trunk/WineCaveSwing/src/be/winecave/repository/CouleurRepository.java
+++ trunk/WineCaveSwing/src/be/winecave/repository/CouleurRepository.java
@@ -6,4 +6,8 @@ import be.winecave.model.Couleur;
 @Repository
 public class CouleurRepository extends BaseRepository<Couleur> {
 
+	public Couleur findByName(String nomCouleur) {
+		return getSingleOrNullResult(em.createQuery("select c from Couleur c where c.nom=:nom").setParameter("nom", nomCouleur));
+	}
+
 }
