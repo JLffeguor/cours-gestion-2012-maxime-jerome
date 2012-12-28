4cc20d6fffeeec5c929355caa552cb8f77bf4e46
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2012-12-28 02:18:07 +0000

    remove nullobjectexception

--- trunk/WineCaveSwing/src/be/winecave/exception/NullObjectException.java
+++
@@ -1,9 +0,0 @@
-package be.winecave.exception;
-
-@SuppressWarnings("serial")
-public class NullObjectException extends RuntimeException {
-
-	public NullObjectException(String message) {
-		super(message);
-	}
-}
