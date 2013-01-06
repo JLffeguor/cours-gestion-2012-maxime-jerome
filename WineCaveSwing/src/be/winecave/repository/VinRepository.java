8e46242b82cb39cf61745e24e4c292c5b7190018
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-06 19:24:33 +0000

    add vinRepository

---
+++ WineCaveSwing/src/be/winecave/repository/VinRepository.java
@@ -0,0 +1,9 @@
+package be.winecave.repository;
+
+import org.springframework.stereotype.Repository;
+
+import be.winecave.model.Vin;
+@Repository
+public class VinRepository extends BaseRepository<Vin> {
+
+}
