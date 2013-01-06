4df9a8ff71cf6de876c3dd1624f8ef56ed0d3bae
ditoro.marco@gmail.com <ditoro.marco@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-06 19:42:29 +0000

    add findAll()

--- WineCaveSwing/src/be/winecave/repository/PaysViticoleRepository.java
+++ WineCaveSwing/src/be/winecave/repository/PaysViticoleRepository.java
@@ -1,13 +1,21 @@
 package be.winecave.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import be.winecave.model.PaysViticole;
 
 @Repository
+@SuppressWarnings("unchecked")
 public class PaysViticoleRepository extends BaseRepository<PaysViticole>{
 
 	public PaysViticole findByName(String name) {
 		return getSingleOrNullResult(em.createQuery("select p from PaysViticole p where p.nom=:nom").setParameter("nom", name));
+	}
+	
+	
+	public List<PaysViticole> findAll() {
+		return em.createQuery("select p from PaysViticole p order by p.nom").getResultList();
 	}
 }
