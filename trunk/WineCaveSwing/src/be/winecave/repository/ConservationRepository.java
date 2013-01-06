46d4d375b318db40df9de30eb98038a4e7bc01cc
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-06 19:24:53 +0000

    fix missing annotation

--- trunk/WineCaveSwing/src/be/winecave/repository/ConservationRepository.java
+++ trunk/WineCaveSwing/src/be/winecave/repository/ConservationRepository.java
@@ -2,9 +2,11 @@ package be.winecave.repository;
 
 import java.util.Date;
 
+import org.springframework.stereotype.Repository;
+
 import be.winecave.exception.NonImplementedException;
 import be.winecave.model.Conservation;
-
+@Repository
 public class ConservationRepository extends BaseRepository<Conservation> {
 	
 	public Conservation findConservation(Date minimum, Date maximum, Date debutApogee, Date finApogee, double temperature){
