984041031973527816d19162f0a0d0843fa6edbb
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-09 17:46:54 +0000

    suppreswarning unchecked

--- trunk/WineCaveSwing/src/be/winecave/repository/CategorieRepository.java
+++ trunk/WineCaveSwing/src/be/winecave/repository/CategorieRepository.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import be.winecave.model.Categorie;
 @Repository
+@SuppressWarnings("unchecked")
 public class CategorieRepository extends BaseRepository<Categorie> {
 
 	public Categorie findByName(String nomCategorie) {
 		return getSingleOrNullResult(em.createQuery("select c from Categorie c where c.nom=:nom").setParameter("nom",nomCategorie));
 	}
+
 
 	public List<Categorie> findAll() {
 		return em.createQuery("select c from Categorie c order by c.nom").getResultList();
