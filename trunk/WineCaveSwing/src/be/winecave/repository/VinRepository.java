f6ba539d3f17e317256ec90ce202d229c0373079
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-09 17:47:22 +0000

    add findallproducteur

--- trunk/WineCaveSwing/src/be/winecave/repository/VinRepository.java
+++ trunk/WineCaveSwing/src/be/winecave/repository/VinRepository.java
@@ -1,9 +1,15 @@
 package be.winecave.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import be.winecave.model.Vin;
 @Repository
 public class VinRepository extends BaseRepository<Vin> {
+	
+	public List<String> findAllProducteur() {
+		return em.createQuery("select producteur from Vin v order by v.producteur").getResultList();
+	}
 
 }
