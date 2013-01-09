f1deebdad09874cceb81f46537249b95686b5c62
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-09 02:19:21 +0000

    some findAll() methods

--- WineCaveSwing/src/be/winecave/repository/CategorieRepository.java
+++ WineCaveSwing/src/be/winecave/repository/CategorieRepository.java
@@ -1,5 +1,7 @@
 package be.winecave.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import be.winecave.model.Categorie;
@@ -8,6 +10,10 @@ public class CategorieRepository extends BaseRepository<Categorie> {
 
 	public Categorie findByName(String nomCategorie) {
 		return getSingleOrNullResult(em.createQuery("select c from Categorie c where c.nom=:nom").setParameter("nom",nomCategorie));
+	}
+
+	public List<Categorie> findAll() {
+		return em.createQuery("select c from Categorie c order by c.nom").getResultList();
 	}
 
 }
