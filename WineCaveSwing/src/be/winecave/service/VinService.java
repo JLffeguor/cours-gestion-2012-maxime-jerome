cdf964be08df996e0dcc78bb1c7bfc46775153b1
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-09 17:46:24 +0000

    add creervin with object as param

--- WineCaveSwing/src/be/winecave/service/VinService.java
+++ WineCaveSwing/src/be/winecave/service/VinService.java
@@ -3,6 +3,9 @@ package be.winecave.service;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,9 @@ import be.winecave.model.Categorie;
 import be.winecave.model.Classement;
 import be.winecave.model.Conservation;
 import be.winecave.model.Couleur;
+import be.winecave.model.PaysViticole;
 import be.winecave.model.Region;
+import be.winecave.model.RegionViticole;
 import be.winecave.model.Vin;
 import be.winecave.repository.BouteilleRepository;
 import be.winecave.repository.CategorieRepository;
@@ -25,6 +30,9 @@ import be.winecave.repository.VinRepository;
 
 @Service
 public class VinService {
+    @PersistenceContext
+    EntityManager em;//only used to check state of entity
+	
 	@Autowired
 	RegionViticoleRepository regionViticoleRepository;
 	@Autowired
@@ -50,7 +58,8 @@ public class VinService {
 						String anneeMinimumConservation,String anneeMaximumConservation,String anneeDebutApogee,String anneeFinApogee,double temperatureMinimum,double temperatureMaximum,
 						Vin vin) {
 		//TODO assert user can do this
-		//TODO check if special categrori "vin de pays , vin de table " user can add specific region but only land
+		//TODO check if special categorie "vin de pays , vin de table " user can add specific region but only land
+		///check right vin parameter
 		if(vin == null){
 			throw new IllegalArgumentException("le paramètre vin ne peut pas être null");
 		}
@@ -59,39 +68,114 @@ public class VinService {
 			throw new IllegalArgumentException("le nom du vin ne doit pas être vide");//TODO créer une exception pour els champs empty ? --maxime 28/12/12
 		}
 		
+		////retrieve entities in db
 		Region region = regionViticoleRepository.findByName(nomRegion);
 		if (region == null) {
 			region = paysViticoleRepository.findByName(nomRegion);
 		}
-		
 		Categorie categorie = categorieRepository.findByName(nomCategorie);
 		Couleur couleur = couleurRepository.findByName(nomCouleur);
 		Bouteille bouteille = bouteilleRepository.findByName(nomBouteille);
 		Classement classement = classementRepository.findByName(nomClassement);
 		
-		SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+		///delegate to second method
+		Vin newVin = creerVin(region, 
+						   	  categorie, 
+						   	  couleur, 
+						   	  bouteille, 
+						   	  classement, 
+						   	  anneeMinimumConservation, anneeMaximumConservation, anneeDebutApogee, anneeFinApogee, temperatureMinimum, temperatureMaximum, 
+						   	  vin);
 		
-		//TODO chercher dans la db une conservation qui correspondrait en tout point à celle donnée
+		return newVin;
+	}
+	
+	public Vin creerVin(Region region, 
+						Categorie categorie,
+						Couleur couleur, 
+						Bouteille bouteille,
+						Classement classement, 
+						String anneeMinimumConservation,String anneeMaximumConservation,String anneeDebutApogee,String anneeFinApogee,double temperatureMinimum,double temperatureMaximum, 
+						Vin vin) {
+		
+		////check in the db if there is already exact same conservation
+		SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
 		Conservation conservation = null;
 		try {
-			conservation = new Conservation(yearFormat.parse(anneeMinimumConservation), yearFormat.parse(anneeMaximumConservation), yearFormat.parse(anneeDebutApogee), yearFormat.parse(anneeFinApogee), temperatureMinimum, temperatureMaximum);
-		} catch (ParseException e) {
+			conservation = conservationRepository.findAnExactConservation(yearFormat.parse(anneeMinimumConservation), yearFormat.parse(anneeMaximumConservation), yearFormat.parse(anneeDebutApogee), yearFormat.parse(anneeFinApogee), temperatureMinimum, temperatureMaximum);
+			if ( conservation == null ) {
+				conservation = new Conservation(yearFormat.parse(anneeMinimumConservation), yearFormat.parse(anneeMaximumConservation), yearFormat.parse(anneeDebutApogee), yearFormat.parse(anneeFinApogee), temperatureMinimum, temperatureMaximum);
+			}
+		} catch (ParseException e1) {
 			throw new IllegalArgumentException("la date de conservation du vin est dans un mauvais format");
 		}
 		
-		Vin result = vin;
+		/////ensure linked entities managed
+		if (region != null) {
+			if(region instanceof RegionViticole) {
+				if(region.getId() == null) {
+					regionViticoleRepository.persist((RegionViticole) region);
+				}
+				region = regionViticoleRepository.merge((RegionViticole) region);
+			} else if (region instanceof PaysViticole) {
+				if(region.getId() == null) {
+					paysViticoleRepository.persist((PaysViticole) region);
+				}
+				region = paysViticoleRepository.merge((PaysViticole) region);
+			}
+			vin.setRegion(region);
+		}
+			
+		if(categorie != null) {
+			if(categorie.getId() == null) {
+				categorieRepository.persist(categorie);
+			}
+			categorie = categorieRepository.merge(categorie);
+			vin.setCategorie(categorie);
+		}
+			
+		if(couleur != null){
+			if(couleur.getId() == null) {
+				couleurRepository.persist(couleur);
+			}
+			couleur = couleurRepository.merge(couleur);
+			vin.setCouleur(couleur);
+		}
+		if(bouteille != null) {
+			if(bouteille.getId() == null) {
+				bouteilleRepository.persist(bouteille);
+			}
+			bouteille = bouteilleRepository.merge(bouteille);
+			vin.setBouteille(bouteille);
+		}
+		if(classement != null) {
+			if(classement.getId() == null) {
+				classementRepository.persist(classement);
+			}
+			classement = classementRepository.merge(classement);
+			vin.setClassement(classement);
+		}
+		if(conservation != null) {
+			if(conservation.getId() == null) {
+				conservationRepository.persist(conservation);
+			}
+			conservation = conservationRepository.merge(conservation);
+			vin.setConservation(conservation);
+		}
 		
-		result.setRegion(region);
-		result.setCategorie(categorie);
-		result.setCouleur(couleur);
-		result.setBouteille(bouteille);
-		result.setClassement(classement);
-		result.setConservation(conservation);
+		///persist or merge new entities
+		if(conservation.getId() == null) {
+			conservationRepository.persist(conservation);
+		}
+		if(vin.getId() == null) {
+			vinRepository.persist(vin);
+		}
 		
-		conservationRepository.persist(conservation);
-		vinRepository.persist(vin);
+		conservationRepository.merge(conservation);
+		vinRepository.merge(vin);
 		
-		return result;
+		return vin;
+
 	}
 	
 	//TODO methode qui supprime le vin car le vin est lié à beacoup d'entités donc supprimé , ou pas , les entités liées
