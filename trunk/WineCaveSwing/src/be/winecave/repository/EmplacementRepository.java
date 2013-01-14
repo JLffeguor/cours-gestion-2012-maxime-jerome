822453424ff42ad4608395929f37c0b3e0eb10f7
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-01-14 16:55:54 +0000

    EmplacementRepository PlaceRepository

--- trunk/WineCaveSwing/src/be/winecave/repository/EmplacementRepository.java
+++ trunk/WineCaveSwing/src/be/winecave/repository/EmplacementRepository.java
@@ -5,5 +5,10 @@ import org.springframework.stereotype.Repository;
 import be.winecave.model.Emplacement;
 @Repository
 public class EmplacementRepository extends BaseRepository<Emplacement> {
+	
+	public Emplacement findByName(Object nom) {
+		return getSingleOrNullResult(em.createQuery("select e from Emplacement e where e.nom=:nom")
+				.setParameter("nom", nom));
+	}
 
 }
