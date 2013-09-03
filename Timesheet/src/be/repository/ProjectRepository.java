76b4c9598ffaeef27e7e39bfe01674aea1f52b42
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-09-03 21:16:44 +0000

    plop

--- Timesheet/src/be/repository/ProjectRepository.java
+++ Timesheet/src/be/repository/ProjectRepository.java
@@ -1,10 +1,17 @@
 package be.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import be.model.Project;
 
 @Repository
 public class ProjectRepository extends BaseRepository<Project> {
+
+	@SuppressWarnings("unchecked")
+	public List<Project> findAll() {
+		return  em.createQuery("select p from Project p").getResultList();
+	}
 
 }
