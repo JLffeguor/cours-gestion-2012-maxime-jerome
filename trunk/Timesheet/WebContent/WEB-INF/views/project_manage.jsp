eb3cbc929020f109f72362d78dd36e5074003592
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-09-03 21:16:44 +0000

    plop

--- trunk/Timesheet/WebContent/WEB-INF/views/project_manage.jsp
+++ trunk/Timesheet/WebContent/WEB-INF/views/project_manage.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.oracle.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,8 @@
 <title>project_manage</title>
 </head>
 <body>
-
+<c:forEach items="${projectList}" var="project">
+	${project.name}
+</c:forEach>
 </body>
 </html>
--- trunk/Timesheet/src/be/controller/ProjectController.java
+++ trunk/Timesheet/src/be/controller/ProjectController.java
@@ -1,17 +1,28 @@
 package be.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import be.model.User;
+import be.repository.ProjectRepository;
 
 
 @Controller
 public class ProjectController extends BaseController<User> {
+	
+	@Autowired
+	ProjectRepository projectRepository;
 
     @RequestMapping("/project_manage")
-    public String projectManage() {
-		return "project_manage";
+    public ModelAndView projectManage() {
+    	ModelAndView mv = new ModelAndView("project_manage");
+//    	if(SecurityContext.isUserHasRole(Role.PROJECT_ADMIN) 
+//    		|| SecurityContext.isUserHasRole(Role.ADMIN)) {
+    			mv.addObject("projectList", projectRepository.findAll());
+//    		}
+		return mv;
     }    
     
     @RequestMapping("/project_activity")
