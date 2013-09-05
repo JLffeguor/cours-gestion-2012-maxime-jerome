f58aec8c03f6bf6a69b218a0fa78653276437299
max.s.1988@gmail.com <max.s.1988@gmail.com@d6d47689-b161-9dc6-059a-6fd6d2ef4020>
2013-09-05 12:17:35 +0000

    fix test creation class

--- Timesheet/src/ProjectCreation.java
+++ Timesheet/src/ProjectCreation.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import be.model.AssignedUser;
 import be.model.Project;
 import be.model.User;
 import be.model.User.Role;
@@ -24,10 +25,18 @@ public class ProjectCreation {
 		
 		((ProjectRepository) ContextUtil.getSpringBean("projectRepository")).persist(project);
 		
-		project.addMember( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plop") , Role.PROJECT_MANAGER);
-		project.addMember( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plep") , Role.USER);
-		project.addMember( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plup") , Role.USER);
-		
+		AssignedUser assignedUser = new AssignedUser();
+		assignedUser.setUser( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plop") );
+		assignedUser.setRole(Role.PROJECT_MANAGER);
+		project.addMember(assignedUser);
+		assignedUser = new AssignedUser();
+		assignedUser.setUser( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plep") );
+		assignedUser.setRole(Role.USER);
+		project.addMember(assignedUser);
+		assignedUser = new AssignedUser();
+		assignedUser.setUser( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plup") );
+		assignedUser.setRole(Role.USER);
+		project.addMember(assignedUser);
 		((ProjectRepository) ContextUtil.getSpringBean("projectRepository")).merge(project);
 		
 		System.out.println("project id" + project.getId());
@@ -36,7 +45,9 @@ public class ProjectCreation {
 		project.setName("projet 2");
 		
 		((ProjectRepository) ContextUtil.getSpringBean("projectRepository")).persist(project);
-		project.addMember(((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plup"), Role.PROJECT_MANAGER);
+		assignedUser = new AssignedUser();
+		assignedUser.setUser( ((UserRepository) ContextUtil.getSpringBean("userRepository")).getUserByUserName("plup") );
+		assignedUser.setRole(Role.PROJECT_MANAGER);
 		((ProjectRepository) ContextUtil.getSpringBean("projectRepository")).merge(project);
 
 		
